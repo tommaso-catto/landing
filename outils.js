52acd6414f5ad68bca0b4c3d89983b6245154cbe
tommaso-catto <75039961+tommaso-catto@users.noreply.github.com>
2021-01-13 16:24:59 +0100

    Add files via upload

---
+++ outils.js
@@ -0,0 +1,9 @@
+function ouvrepopup() {
+	document.getElementById("popup").style.opacity="1";
+}
+
+function closepopup () {
+	document.getElementById("btnClose").style.opacity="0";
+}
+
+setTimeout(function ouvrepopup(), 15000);
